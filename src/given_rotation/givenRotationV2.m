c58f5915abb4a6cc7df141d88f5c0f601dbd5f57
Faizal Adhitama <faizaladhitamaprabowo@gmail.com>
2017-03-12 19:47:04 +0700

    Algoritma lebih baik dari V1. Lebih cepat berkali kali lipat bila matrixnya besar. Akurasi masih belum diperbaiki

---
+++ src/given_rotation/givenRotationV2.m
@@ -0,0 +1,48 @@
+function [Q,R] = givenRotationV2(A)
+%GivenRotation melakukan dekomposisi matrix A
+%   Input  = matrix A nxn
+%   Output = matrix Q mxn
+%            matrix R nxn
+%   V2 mempercepat perhitungan karena hanya baris
+%   yang melakukan rotasi saja yang mendapatkan operasi
+%   perhitungan (berlaku untuk matrix besar)
+%   contoh : di matriks 10x10 mulai terlihat bedanya
+%   Lebih cepat dibandingkan householder tapi akurasi lebih rendah
+    tic
+    [m,n] = size(A);
+    identitas = eye(m);
+    a = 1;
+    temp = A;
+    temp2 = identitas;
+    for i=n:-1:1
+        cond = 0;
+        if m > n
+            cond = m-i;
+        else
+            cond = m-i+2;
+        end
+        for j=m:-1:cond
+            e1 = temp(j,a);
+            e2 = temp(j-1,a);
+            r = sqrt(e1^2+e2^2);
+            c = e2/r;
+            s = e1/r;
+            identitas(j,j) = c;
+            identitas(j-1,j-1) = c;
+            identitas(j-1,j) = s;
+            identitas(j,j-1) = -s;
+            temp3 = temp;
+            for k=1:n
+                temp3(j,k) = identitas(j,:)*temp(:,k);
+                temp3(j-1,k) = identitas(j-1,:)*temp(:,k);
+            end
+            temp = temp3;
+            temp2 = temp2*transpose(identitas);
+            identitas = eye(m);
+        end
+        a = a + 1;
+    end
+    R = temp;
+    Q = temp2;
+    toc
+end
