2ecf1bc1113d21df137f3238754edffb2b86e6a3
Faizal Adhitama <faizaladhitamaprabowo@gmail.com>
2017-03-12 20:07:23 +0700

    Algoritma sama dengan givenRotationV2.m hanya ditambah pivoting

--- src/given_rotation/givenRotationWPivotingV2.m
+++ src/given_rotation/givenRotationWPivotingV2.m
@@ -3,6 +3,7 @@ function [Q,R,p] = givenRotationWPivotingV2(A)
 %   Input  = matrix A nxn
 %   Output = matrix Q mxn
 %            matrix R nxn
+%            matrix P matrix elementer untuk mengubah b menjadi sesuai
 %   Menggunakan pivoting = menambah akurasi mengurangi kecepatan
     tic
     [m,n] = size(A);
@@ -10,22 +11,27 @@ function [Q,R,p] = givenRotationWPivotingV2(A)
     a = 1;
     temp = A;
     temp2 = identitas;
-    zero = zeros(m,1)
+    zero = zeros(m,1);
     p = eye(m);
     for i=n:-1:1
-        zero(a:m,1) = A(a:m,a);
-        [c,z] = max(abs(zero))
+        zero(a:m,1) = temp(a:m,a);
+        [c,z] = max(abs(zero));
         if c==0
             quit;
         end
-        tmpA = A(a,:);
+        tmpA = temp(a,:);
         tmpp = p(a,:);
-        A(a,:) = A(z,:);
+        temp(a,:) = temp(z,:);
         p(a,:) = p(z,:);
-        A(z,:) = tmpA;
+        temp(z,:) = tmpA;
         p(z,:) = tmpp;
-        A
-        for j=m:-1:m-i
+        cond = 0;
+        if m > n
+            cond = m-i;
+        else
+            cond = m-i+2;
+        end
+        for j=m:-1:cond
             e1 = temp(j,a);
             e2 = temp(j-1,a);
             r = sqrt(e1^2+e2^2);
@@ -35,7 +41,12 @@ function [Q,R,p] = givenRotationWPivotingV2(A)
             identitas(j-1,j-1) = c;
             identitas(j-1,j) = s;
             identitas(j,j-1) = -s;
-            temp = identitas*temp;
+            temp3 = temp;
+            for k=1:n
+                temp3(j,k) = identitas(j,:)*temp(:,k);
+                temp3(j-1,k) = identitas(j-1,:)*temp(:,k);
+            end
+            temp = temp3;
             temp2 = temp2*transpose(identitas);
             identitas = eye(m);
         end
