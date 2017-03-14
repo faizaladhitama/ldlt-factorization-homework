a7bac6244a7b66d223f2770b3e6b7dece8829196
damartanjung <damartanjung@gmail.com>
2017-03-14 22:23:54 +0700

    Add laporan

--- src/householder/householder.m
+++ src/householder/householder.m
@@ -1,4 +1,4 @@
-% A merupakan matriks persegi
+% A merupakan matriks m x n
 % b merupakan matriks n x 1
 % C merupakan matriks augmented dari A dan b yang sudah dilakukan householder, 
 % dimana A merupakan mastriks segitiga atas
@@ -9,9 +9,7 @@ for i = 1:(n-1)
     vw = C(i:m,i) + sign(C(i,i)) * norm(C(i:m,i))*[1;zeros(m-i,1)];
     alfa = 2/(vw'*vw);
     for j=i:n+1
-        C(i:m,j) = C(i:m,j) - alfa*vw'*C(i:m,j)*vw;
+        C(i:m,j) = C(i:m,j) - (alfa*vw'*C(i:m,j))*vw;
     end
 end
 C
-D = rref(C);
-D
